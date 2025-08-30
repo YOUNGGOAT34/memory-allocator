c812279400a9243edeeb4d1b38ded8a507bc83b9
GOAT <lucasonyiego283@gmail.com>
2025-08-30 16:08:42 +0300

    defining the necessary data types

---
+++ alloc_mem.h
@@ -0,0 +1,21 @@
+#ifndef ALLOC_MEM_H
+#define ALLOC_MEM_H
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<stdbool.h>
+
+typedef signed char i8;
+typedef signed short int i16;
+typedef signed int i32;
+typedef signed long int i64;
+
+typedef unsigned char u8;
+typedef unsigned short int u16;
+typedef unsigned int u32;
+typedef unsigned long int u64;
+
+
+
+
+#endif
